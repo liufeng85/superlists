09c5cfa548793b3756ee8be5ad249f897e254b06
liufeng85 <liufeng85@vip.qq.com>
2015-11-02 08:24:59 +0800

    修改测试代码

--- src/lists/tests.py
+++ src/lists/tests.py
@@ -14,4 +14,3 @@ class HomePageTest(TestCase):
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
-
